2517d291fe267f014d90ba17b27fd2d97a5ab554
v-belikova <v.belikova@dunice.net>
2022-05-04 18:14:04 +0300

    Task

--- Task2.java
+++ Task2.java
@@ -1,13 +1,15 @@
 package test;
-
+/*Создать консольное приложение которое
+будет при нимать 2 любых числа и показывать какое больше
+ */
 import java.util.Scanner;
 class Task2 {
     public static void main(String[] args) {
-        System.out.println("Введите любые два целых числа: ");
+        System.out.println("Введите любое число: ");
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt();
+        System.out.println("Введите любое число: ");
         int number2 = scan.nextInt();
         System.out.println(Math.max(number, number2));
-        33
     }
 }
--- Task3.java
+++ Task3.java
@@ -1,4 +1,17 @@
 package test;
+/*Создать консольное приложение в котором можно задать минимальное
+и максимальное число, после чего в консоль выводятся
+все простые числа в указанном промежутке
+ */
+import java.util.Scanner;
 
 public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Введите любое число: ");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.println("Введите любое число: ");
+        int number2 = scan.nextInt();
+        System.out.println(Math.max(number, number2));
+    }
 }
