4c50af0f59032fcf8e7b75b7c8ca92c3a2a1e19e
v-belikova <v.belikova@dunice.net>
2022-05-05 17:56:14 +0300

    Task2

--- Task4.java
+++ Task4.java
@@ -3,6 +3,7 @@ package test;
 корзина, в корзину  можно добавлять продукты а затем выводить их все в консоль
  */
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Task4 {
 
 }
--- Task5.java
+++ Task5.java
@@ -2,5 +2,12 @@ package test;
 /*Создать консольное приложение, которое при старте попросит указать пароль и далее будет спрашивать его, пока не будет введен верный, после ввода верного пароля
 программа закончит свое выполнение,   при вводе некорректного пароля, будет просьба ввести новый
  */
+import java.util.Scanner;
 public class Task5 {
+    public static void main(String[] args) {
+
+        Scanner user_input = new Scanner(System.in);
+        System.out.println("Введите пароль: ");
+        String first_name = user_input.next();
+    }
 }
